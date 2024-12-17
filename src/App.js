e8ffc063404962fd533585e961e54dbbee0d03f3
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-17 20:01:50 +0545

    Bootstrap v5 added on my GalleryX react project.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to GalleryX</h1>
-    </div>
+    <>
+      <div className="App">
+        <div className="card" style={{ width: '18rem' }}>
+          <img src="https://source.unsplash.com/girl" className="card-img-top" alt="image"/>
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
