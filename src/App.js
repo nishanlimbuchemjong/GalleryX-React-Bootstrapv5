871fdbd6b1fe7c24d552f2db6bfa6bf9f45a4200
nishanlimbuchemjong <nishanlimbu.chemjong2000@gmail.com>
2024-12-18 17:32:30 +0545

    3 columns per rows images displayed.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageCard from './components/ImageCard';
 
 function App() {
@@ -8,25 +7,27 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
-  useEffect(()=>{
-    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
-    .then(res => res.json())
-    .then(data => {
-      setImages(data.hits);
-      setIsLoading(false);
-    })
-    .catch(err => console.log(err));
-  })
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch((err) => console.log(err));
+  }, [term]);
+
   return (
-    <>
-      <div className="container">
-        <div>
-          {images.map(image=>(
-            <ImageCard key={image.id} image={image}/>
-          ))}
-        </div>
+    <div className="container mt-4">
+      <div className="row">
+        {/* Image Cards */}
+        {images.map((image) => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,20 +1,22 @@
-import React from 'react'
+import React from 'react';
 
 const ImageCard = ({ image }) => {
   return (
-    <div className="card" style={{ width: '18rem' }}>
-        <img src={image.webformatURL} className="card-img-top" alt="image"/>
+    <div className="col-md-4 mb-4">
+      <div className="card">
+        <img src={image.webformatURL} className="card-img-top" alt="image" />
         <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text"> photo by: {image.user}</p>
-        <p className="card-text"> Views: {image.views}</p>
-        <p className="card-text"> Downloads: {image.downloads}</p>
-        <p className="card-text"> Likes: {image.likes}</p>
-        <p className="card-text"> Comments: {image.comments}</p>
-        <a href="#" className="btn btn-primary">Go somewhere</a>
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Photo by: {image.user}</p>
+          <p className="card-text">Views: {image.views}</p>
+          <p className="card-text">Downloads: {image.downloads}</p>
+          <p className="card-text">Likes: {image.likes}</p>
+          <p className="card-text">Comments: {image.comments}</p>
+          <a href="#" className="btn btn-primary">Go somewhere</a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ImageCard
+export default ImageCard;
